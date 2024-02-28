dad1aaed11853048536f84c91f616b3b2f85fd27
iwguy426 <iwguy426@yahoo.co.jp>
2024-02-28 09:46:07 +0900

    Propsを使ってコンポーネントの表示を出し分け

--- components/Headline.js
+++ components/Headline.js
@@ -1,14 +1,14 @@
 import styles from "@/styles/Home.module.css";
 
 
-export function Headline() {
+export function Headline(props) {
     return ( 
 <div>
-    <h1 className={styles.title}>Test Page</h1><br />
+    <h1 className={styles.title}>{props.title}</h1><br />
 
     <p className={styles.description}>
     Get started by editing{" "}
-    <code ClassName={styles.code}>pages/test/test.js</code>
+    <code ClassName={styles.code}>pages{props.page}.js</code>
     </p>
 </div>
     );
